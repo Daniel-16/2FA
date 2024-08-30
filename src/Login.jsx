8ffc43b5228e9d4758064ca5ad09948156e622f4
Daniel-16 <danieloloruntoba681@gmail.com>
2024-08-30 21:45:07 +0100

    Set up React project with Firebase, add signup and login functionality

---
+++ src/Login.jsx
@@ -0,0 +1,82 @@
+import "./App.css";
+import { useState } from "react";
+import { auth } from "../config/firebaseConfig.js";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      console.log("User logged in successfully");
+    } catch (error) {
+      setError(error.message);
+      console.error("Error logging in:", error.message);
+      setTimeout(() => {
+        setError(null);
+      }, 5000);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold mb-4">2FA Project</h1>
+      <h2 className="text-3xl font-bold mb-4">Login</h2>
+      {error && <p className="text-red-500 mt-4">{error}</p>}
+      <form className="mt-8 space-y-6 w-full max-w-md" onSubmit={handleLogin}>
+        <div>
+          <label htmlFor="email" className="sr-only">
+            Email address
+          </label>
+          <input
+            id="email"
+            value={email}
+            name="email"
+            type="email"
+            autoComplete="email"
+            required
+            className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            placeholder="Email address"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password" className="sr-only">
+            Password
+          </label>
+          <input
+            id="password"
+            name="password"
+            value={password}
+            type="password"
+            autoComplete="current-password"
+            required
+            className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <button
+            type="submit"
+            className={`group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${
+              loading ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+            disabled={loading}
+          >
+            {loading ? "Loading..." : "Login"}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
